7a1a26c836aeb8e1b58fa8f316efdb07de32fdc8
andre <apracapinheiro@yahoo.com.br>
2017-01-31 14:22:33 -0300

    - Funcoes separadas em uma biblioteca a parte. - Adicionado cerveja da casa - Trabalhando com o site Arte Brew

---
+++ insumos.py
@@ -0,0 +1,29 @@
+from scrapingtools import *
+
+
+# NOME_SITE = 'LAMAS'
+# NOME_SITE = 'BREWHEAD'
+# NOME_SITE = 'WE'
+# NOME_SITE = 'CERVEJADACASA'
+NOME_SITE = 'ARTEBREW'
+
+# headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5)'}  # MAC
+# headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'} # CHROME
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0'}  # FIREFOX
+
+wb = openpyxl.Workbook()
+sheet = wb.get_active_sheet()
+sheet.title = 'Precos maltes ' + NOME_SITE
+
+
+num_pagina = 1
+# url = 'http://loja.lamasbrewshop.com.br/insumos/malte-cereais.html?' + 'p=%d' % num_pagina  # LAMAS
+# url = 'https://brewheadshop.com.br/insumos/maltes?' + 'page=%d' % num_pagina  # BREWHEAD
+# url = 'http://loja.weconsultoria.com.br/maltes-s10038/?' + 'pagina=%d' % num_pagina  # WE
+# url = 'http://www.cervejadacasa.com/loja/catalogo.php?loja=420472&categoria=55&pg=%d' % num_pagina # CERVEJA DA CASA
+url = 'http://cervejacaseira.com.br/materias-primas.html?cat=23&p=%d' %num_pagina  # ARTEBREW
+lista_insumos = []
+
+requisicao, status = request_site(url, headers, num_pagina)
+lista_precos_insumos = scraping_insumos(requisicao, status, num_pagina, lista_insumos, headers, NOME_SITE)
+gera_planilha(lista_insumos, wb, sheet, NOME_SITE)
---
+++ scrapingtools.py
@@ -0,0 +1,120 @@
+import time
+
+import bs4
+import requests
+import openpyxl
+from openpyxl.styles import Font, NamedStyle
+
+
+def request_site(url, headers, num_pagina):
+    """
+    Realiza a requisicao da pagina
+    :param url: url da página que será realizado o scraping
+    :param headers: cabeçalhos contendo informacoes sobre o browser. Necessario para passar pela validacao no servidor
+    alvo
+    :param num_pagina:
+    :return:
+    """
+    if not num_pagina:
+        num_pagina = 1
+    time.sleep(3)
+    res = requests.get(url, headers=headers)
+    status_request = res.raise_for_status()
+    return res, status_request
+
+
+def scraping_insumos(requisicao, status, num_pagina, lista_insumos, headers, site):
+    """
+    Funcao que realiza o scraping dos insumos.
+    :param requisicao: objeto requisicao da pagina
+    :param status: status da requisicao, só continua se o valor for None. Valores diferentes de None indicam o erro
+    que o servidor enviou
+    :param num_pagina: numero da pagina que sera realizado o scraping
+    :param lista_insumos: lista que contem os insumos das outras paginas que ja passaram por scraping
+    :param cabecalho enviado para o servidor alvo
+    :param url do site que será realizado scraping
+    :return: lista de insumos com todas as paginas.
+    """
+    while status == None:
+        if site == 'BREWHEAD':
+            pagina = bs4.BeautifulSoup(requisicao.text)
+            insumo = pagina.select('div h4')
+            precos = pagina.select('p[class="price"]')
+            proximo = pagina.select('ul[class="pagination"]')
+            url = 'https://brewheadshop.com.br/insumos/maltes?' + 'page=%d' % num_pagina  # BREWHEAD
+        elif site == 'LAMAS':
+            pagina = bs4.BeautifulSoup(requisicao.text)
+            insumo = pagina.select('div[class="list-conteiner-name"]')
+            precos = pagina.select('span[class="regular-price"]')
+            proximo = pagina.select('a[class="next i-next"]')
+            url = 'http://loja.lamasbrewshop.com.br/insumos/malte-cereais.html?' + 'p=%d' % num_pagina  # LAMAS
+        elif site == 'WE':
+            pagina = bs4.BeautifulSoup(requisicao.text)
+            insumo = pagina.select('div[class="product-name"]')
+            precos = pagina.select('b[class="sale"]')
+            proximo = pagina.select('span[class="page-next"]')
+            url = 'http://loja.weconsultoria.com.br/maltes-s10038/?' + 'pagina=%d' % num_pagina  # WE
+        elif site == 'CERVEJADACASA':
+            pagina = bs4.BeautifulSoup(requisicao.text)
+            insumo = pagina.select('div[itemprop="name"]')
+            precos = pagina.select('meta[itemprop="price"]')
+            proximo = pagina.select('a[rel="next"]')
+            url = 'http://www.cervejadacasa.com/loja/catalogo.php?loja=420472&categoria=55&pg=%d'
+        elif site == 'ARTEBREW':
+            pagina = bs4.BeautifulSoup(requisicao.text)
+            insumo = pagina.select('tr h5')
+            precos = pagina.select('span[class="regular-price"]')
+            proximo = pagina.select('a[class="next i-next"]')
+            url = 'http://cervejacaseira.com.br/materias-primas.html?cat=23&p=%d' % num_pagina  # ARTEBREW
+
+        if len(precos) > 0:
+            for i in range(len(precos)):
+                if site == 'CERVEJADACASA':
+                    nome_insumo = insumo[i].get_text()
+                    preco_insumo = str(precos[i].attrs['content'])
+                    insumos_preco = {nome_insumo: preco_insumo}
+                    lista_insumos.append(insumos_preco)
+                else:  # BREWHEAD, LAMAS, WE
+                    nome_insumo = insumo[i].get_text().strip()
+                    preco_insumo = precos[i].get_text().strip()
+                    # if not esgotado[i].get_text() == 'Esgotado':
+                    insumos_preco = {nome_insumo: preco_insumo}
+                    lista_insumos.append(insumos_preco)
+
+            if len(proximo) > 0:
+                num_pagina += 1
+                url = url % num_pagina
+                requisicao, status = request_site(url, headers, num_pagina)
+                # res = requests.get(url, headers=headers)
+                # status_request = res.raise_for_status()
+            else:
+                break
+        else:
+            break
+
+    return lista_insumos
+
+
+def gera_planilha(lista_insumos, wb, planilha, site):
+    """
+    Funcao que exporta a lista de insumos para uma planilha no formato xlsx
+    :param lista_insumos: a lista com os insumos depois do scraping
+    :param wb: WORKBOOK, arquivo principal da planilha
+    :param planilha: sheet (planilha) dentro do WORKBOOK
+    :param site: nome do site que está sendo realizado o scraping
+    :return: sem retorno. Grava a planilha.
+    """
+    fonte = Font(bold=True, size=14)
+    planilha['A1'].font = fonte
+    planilha['A1'] = 'Insumo'
+    planilha['B1'].font = fonte
+    planilha['B1'] = 'Preço'
+
+    for numeroLinha in range(len(lista_insumos)):
+        insumo_preco = str(lista_insumos[numeroLinha]).split(":")
+        # insere na primeira coluna o nome dos insumos
+        planilha.cell(row=numeroLinha+2, column=1).value = insumo_preco[0].replace('{', '').replace('\'', '').replace('\\n', '')
+        # insere na segunda coluna o preco dos insumos
+        planilha.cell(row=numeroLinha+2, column=2).value = insumo_preco[1].replace('\'', '').replace('\' ', '').replace('}', '')
+
+    wb.save(site + '.xlsx')
